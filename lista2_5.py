1b38b9c4115a7f9794fa2ba08e5753dc47aa4dcf
Maria Beatriz <mbms@cesar.school>
2024-09-20 20:48:19 -0300

    Update lista2_5.py

--- lista2_5.py
+++ lista2_5.py
@@ -1,4 +1,7 @@
 numero = int(input("Digite um número inteiro:"))
+inicio = int(input("Informe o inicio"))
+fim = int(input("Informe o fim"))
 
-for i in range(1, 10+1):
+ 
+for i in range(inicio, fim+1):
     print (f"{numero} x {i} = {numero*i}")
